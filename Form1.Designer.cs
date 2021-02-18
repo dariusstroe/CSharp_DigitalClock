f7e7bc79fe6d6da6df92934132e2c605e73f6295
Darius <72557469+dariusstroe@users.noreply.github.com>
2021-02-18 14:20:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDayOfWeek = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Font = new System.Drawing.Font("Segoe MDL2 Assets", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(130, 124);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(151, 66);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "22:22";
+            this.lblTime.TabIndexChanged += new System.EventHandler(this.Timer_Tick);
+            this.lblTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeconds.Font = new System.Drawing.Font("Segoe MDL2 Assets", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.ForeColor = System.Drawing.Color.White;
+            this.lblSeconds.Location = new System.Drawing.Point(285, 124);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(84, 66);
+            this.lblSeconds.TabIndex = 1;
+            this.lblSeconds.Text = "22";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Font = new System.Drawing.Font("Segoe MDL2 Assets", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(12, 298);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(346, 66);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "JULY 28 2014";
+            this.lblDate.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblDayOfWeek
+            // 
+            this.lblDayOfWeek.AutoSize = true;
+            this.lblDayOfWeek.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDayOfWeek.Font = new System.Drawing.Font("Segoe MDL2 Assets", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDayOfWeek.ForeColor = System.Drawing.Color.White;
+            this.lblDayOfWeek.Location = new System.Drawing.Point(396, 298);
+            this.lblDayOfWeek.Name = "lblDayOfWeek";
+            this.lblDayOfWeek.Size = new System.Drawing.Size(285, 66);
+            this.lblDayOfWeek.TabIndex = 3;
+            this.lblDayOfWeek.Text = "Wednesday";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(687, 397);
+            this.Controls.Add(this.lblDayOfWeek);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDayOfWeek;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        public ContentAlignment ToolBarTextAlign { get; private set; }
+        public ContentAlignment TextAlign { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            AutoSize = false;
+            TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            AutoSize = false;
+            ToolBarTextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm");
+            lblSeconds.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MMM dd yyyy");
+            lblDayOfWeek.Text = DateTime.Now.ToString("dddd");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+    }
+}
